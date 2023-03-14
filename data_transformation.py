e9c17c11bcd2d0f42c5f6144771843c1a078e43b
JaredBeluzi <126320399+JaredBeluzi@users.noreply.github.com>
2023-03-14 14:19:07 +0100

    Update data_transformation.py

--- data_transformation.py
+++ data_transformation.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 
 # 1. Import data
-# load logging.csv into a pandas dataframe with ansii encoding and no header
-df = pd.read_csv('logging.csv', encoding='ansi', header=None)
+# load logging.csv into a pandas dataframe with Microsoft cmd encoding and no header
+df = pd.read_csv('logging.csv', encoding='cp852', header=None)
 # name first column "Prozess"
 df.columns = ['process',
               'loop_number_of_date',
@@ -17,8 +17,6 @@ df.columns = ['process',
 df['logging_date'] = df['logging_date'].str[:-2]
 # remove last 2 characters from Ram usage
 df['RAM_usage_KB'] = df['RAM_usage_KB'].str[:-2]
-# replace wrong Ä character in user column
-df['user'] = df['user'].str.replace('Ž', 'Ä')
 # remove dot in ram usage
 df['RAM_usage_KB'] = df['RAM_usage_KB'].str.replace('.', '')
 # cast ram usage to int
@@ -37,8 +35,8 @@ df_grouped = df_grouped.agg({'logging_time':'min',
 # check if file exists
 try:
     # if file exists, append data to existing csv file with ansi encoding
-    df_grouped.to_csv('logging_hist.csv', mode='a', encoding='ansi', header=False)
+    df_grouped.to_csv('logging_hist.csv', mode='a', encoding='utf-8', header=False)
 except FileNotFoundError:
     # if file does not exist, create file and write data to it
-    df_grouped.to_csv('logging_hist.csv', mode='w', encoding='ansi', header=True)
+    df_grouped.to_csv('logging_hist.csv', mode='w', encoding='utf-8', header=True)
 
